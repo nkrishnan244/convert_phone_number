39a23713abf5b2534c63a04fcb2a2fec991f80ad
Nikhil Krishnan <nkrishnan244@gmail.com>
2019-06-25 16:12:54 -0400

    included dictionaries

--- convert_phone_num.py
+++ convert_phone_num.py
@@ -1,36 +1,45 @@
+from create_dictionaries import get_dicts
 
-def number_to_words(raw_phone_number):
-    """
-    Converts string representation of phone number to "wordified" phone number
 
-    Parameters:
-    raw_phone_number (String): string that represents phone number (with dashes)
+class ConvertPhoneNumber:
+    def __init__(self):
+        self.letter2num, self.num2letter = get_dicts()
 
-    Returns:
-    String: string representation of "wordified" phone number
+    def number_to_words(self, raw_phone_number):
+        """
+        Converts string representation of phone number to "wordified" phone number
 
-    """
+        Parameters:
+        raw_phone_number (String): string that represents phone number (with dashes)
 
-def words_to_number(wordified_phone_number):
-    """
-    Converts string representation of "wordified" phone number to normal phone number
+        Returns:
+        String: string representation of "wordified" phone number
 
-    Parameters:
-    wordified_phone_number (String): string that represents "wordified" phone number
+        """
 
-    Returns:
-    String: string representation of phone number
+    def words_to_number(self, wordified_phone_number):
+        """
+        Converts string representation of "wordified" phone number to normal phone number
 
-    """
+        Parameters:
+        wordified_phone_number (String): string that represents "wordified" phone number
 
-def all_wordifications(raw_phone_number):
-    """
-    Converts string representation of phone number to all possible "wordified" phone numbers
+        Returns:
+        String: string representation of phone number
 
-    Parameters:
-    raw_phone_number (String): string that represents phone number (with dashes)
+        """
 
-    Returns:
-    List: List of strings that are all possible "wordified" versions of input phone number
+    def all_wordifications(self, raw_phone_number):
+        """
+        Converts string representation of phone number to all possible "wordified" phone numbers
 
-    """
+        Parameters:
+        raw_phone_number (String): string that represents phone number (with dashes)
+
+        Returns:
+        List: List of strings that are all possible "wordified" versions of input phone number
+
+        """
+
+
+ConvertPhoneNumber()
