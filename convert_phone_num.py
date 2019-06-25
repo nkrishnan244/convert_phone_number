29bc3f018d3280087e55bbf119630a207d604d3e
Nikhil Krishnan <nkrishnan244@gmail.com>
2019-06-25 15:55:01 -0400

    first commit

---
+++ convert_phone_num.py
@@ -0,0 +1,36 @@
+
+def number_to_words(raw_phone_number):
+    """
+    Converts string representation of phone number to "wordified" phone number
+
+    Parameters:
+    raw_phone_number (String): string that represents phone number (with dashes)
+
+    Returns:
+    String: string representation of "wordified" phone number
+
+    """
+
+def words_to_number(wordified_phone_number):
+    """
+    Converts string representation of "wordified" phone number to normal phone number
+
+    Parameters:
+    wordified_phone_number (String): string that represents "wordified" phone number
+
+    Returns:
+    String: string representation of phone number
+
+    """
+
+def all_wordifications(raw_phone_number):
+    """
+    Converts string representation of phone number to all possible "wordified" phone numbers
+
+    Parameters:
+    raw_phone_number (String): string that represents phone number (with dashes)
+
+    Returns:
+    List: List of strings that are all possible "wordified" versions of input phone number
+
+    """
