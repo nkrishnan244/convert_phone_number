62d63300eafd5e8b8592d6f6f9293a92a4c32cd9
Nikhil Krishnan <nkrishnan244@gmail.com>
2019-06-25 21:23:14 -0400

    Finds multiple words at end of phone #

--- convert_phone_num.py
+++ convert_phone_num.py
@@ -6,7 +6,8 @@ class ConvertPhoneNumber:
     def __init__(self):
         self.letter2num, self.num2letter = get_dicts()
         self.word_list = get_word_list()
-        print(self.number_to_words("1-800-724-6837"))
+        self.total_words = []
+        print(self.all_wordifications("1-804-232-7326"))
         print(self.words_to_number("1-800-fgh-6837"))
 
     def number_to_words(self, raw_phone_number):
@@ -66,6 +67,13 @@ class ConvertPhoneNumber:
                 phone_number += wordified_phone_number[i]
         return phone_number
 
+    def find_substr(self, num, curr_string):
+        for i in range(len(num) - 1, -1, -1):
+            words = self.find_all_words(num[i:len(num)])
+            if words:
+                for word in words:
+                    self.total_words.append(word + curr_string)
+                    self.find_substr(num[0:i], word + curr_string)
 
     def all_wordifications(self, raw_phone_number):
         """
@@ -78,30 +86,44 @@ class ConvertPhoneNumber:
         List: List of strings that are all possible "wordified" versions of input phone number
 
         """
-        num_elements = len(raw_phone_number)
-        curr_string = ""
-        num_dashes = -1
-        words = []
-        for i in range(num_elements - 1, -1, -1):  # Iterate backwards through phone number
-            if not raw_phone_number[i] == '-':  # Only consider wordifying elements after a dash
-                curr_string = raw_phone_number[i] + curr_string
-            else:  # If we reach a dash
-                num_dashes += 1  # Used for parsing output
-                string_to_list = []
-                for element in curr_string:
-                    if element not in self.num2letter:  # IF we have an element that can't be converted to a letter
-                        return words
-                    string_to_list.append(self.num2letter[element])  # Convert each number into a list of letters
-                all_combinations = list(product(*string_to_list))  # Find all possible combinations of letters
-                for char_list in all_combinations:
-                    curr_word = ''.join(char_list)  # Convert combination of letters into string
-                    if curr_word in self.word_list:  # Check if the word exists
-                        length = len(curr_word)
-                        # Create and store wordified number
-                        wordified_num = raw_phone_number[0:num_elements - length - num_dashes] + curr_word.upper()
-                        words.append(wordified_num)
-        return words
+        dashless_phone_number = raw_phone_number.replace('-', '')
 
+        for i in range(len(dashless_phone_number) - 1, -1, -1):
+            if dashless_phone_number[i] not in self.num2letter:
+                left_most_element = i + 1
+                break
+
+        reduced_phone_num = dashless_phone_number[left_most_element:]
+        self.find_substr(reduced_phone_num, "")
+
+
+    def find_all_words(self, numbers):
+        """
+        Converts string representation of number to all possible words
+
+        Parameters:
+        numbers (String): string that represents number
+
+        Returns:
+        List: List of strings that are all possible "wordified" versions of number
+
+        """
+        string_to_list = []
+        words = []
+        for element in numbers:
+            if element not in self.num2letter:  # IF we have an element that can't be converted to a letter
+                return words
+            string_to_list.append(self.num2letter[element])  # Convert each number into a list of letters
+        all_combinations = list(product(*string_to_list))  # Find all possible combinations of letters
+        for char_list in all_combinations:
+            curr_word = ''.join(char_list)  # Convert combination of letters into string
+            # For some reason every dictionary includes the letters of the alphabet
+            if curr_word in self.word_list and len(curr_word) > 2:  # Check if the word exists (size for readability)
+            # if curr_word in self.word_list and (curr_word == 'a' or len(curr_word) > 1):  # Check if the word exists
+                length = len(curr_word)
+                # Create and store wordified number
+                words.append(curr_word)
+        return words
 
 
 
